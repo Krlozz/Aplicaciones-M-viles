e72ccd433d6b20b43a7361f5f3e46ce7495514b3
Krlozz Ayala <k_arlos210@hotmail.com>
2018-08-16 01:31:46 -0500

    Segundo avance de proyecto
    Prueba de fragmentos
    Dos tipos de estilo de fragmentos
    Paso de un estilo al otro

---
+++ Segundo avance proyecto/SegundaParte/app/src/main/java/com/carlosayalat/segundaparte/Galeria.java
@@ -0,0 +1,122 @@
+package com.carlosayalat.segundaparte;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link Galeria.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link Galeria#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class Galeria extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+    String datos[];
+    ListView miLista;
+    ArrayAdapter<String> adapter;
+
+    private OnFragmentInteractionListener mListener;
+
+    public Galeria() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment Galeria.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static Galeria newInstance(String param1, String param2) {
+        Galeria fragment = new Galeria();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_galeria, container, false);
+        miLista = (ListView) view.findViewById(R.id.miLista);
+        cargarLista();
+        adapter = new ArrayAdapter<>(getContext(),R.layout.support_simple_spinner_dropdown_item,datos);
+        miLista.setAdapter(adapter);
+
+        return view;
+    }
+
+    private void cargarLista() {
+        datos = new String[] {"hello","dato2","dato3","dato4","dato5","dato6","dato7","dato8","dato9","dato10","dato11","dato12","dato13","dato14"};
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
